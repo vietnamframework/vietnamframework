53fd646a9902b2d5ef17e95772ebfac0fde55f5d
Thachphong <whirlwind2887@gmail.com>
2016-03-18 18:24:37 +0700

    add table

--- love.sql
+++ love.sql
@@ -157,3 +157,21 @@ CREATE TABLE `user` (
 INSERT INTO `user` VALUES ('1', 'admin', '123456', '2015-10-20 11:40:01');
 INSERT INTO `user` VALUES ('2', 'hungbu', '202cb962ac59075b964b07152d234b70', '2015-10-28 09:25:49');
 INSERT INTO `user` VALUES ('3', 'hungbuit', '202cb962ac59075b964b07152d234b70', '2015-10-28 09:29:21');
+
+-- ----------------------------
+-- Table structure for `download_structure`
+-- ----------------------------
+DROP TABLE IF EXISTS `download_structure`;
+CREATE TABLE `download_structure` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Title or Content or Image',
+  `description` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `xpath` varchar(300) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ref_link` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'webiste download',
+  `element_remove` int(11) DEFAULT '0' COMMENT 'when is 1 then remove elemte from html',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of download_systax
+-- ----------------------------
