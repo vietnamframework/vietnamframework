e374ceb8d2b2b3c157575a278395ea5b1e51945e
vietnamframework <admin@vietnamframework.com>
2016-04-04 13:38:56 +0900

    active menu backend

---
+++ app/view/backend/js/alluse.js
@@ -0,0 +1,14 @@
+var url_curent = window.location.href;
+
+$(document).ready(function(){
+	$(".sidebar-menu li a").each(function(){
+		var text_search = "/" + $(this).attr('href');
+		n = url_curent.search(text_search);
+		if(n != -1) {
+			$(this).parent('li').addClass('active');
+		} else {
+			$(this).parent('li').removeClass('active');
+		}
+	});
+});
+
